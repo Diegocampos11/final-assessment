0658d8af01ff304d5313078916adac8b3f39e397
DiegoMauricio.Campos <diegomauricio.campos@solera.com>
2022-11-17 14:46:18 +0100

    adding update to controller

--- springbootproject/src/main/java/com/finalassesment/springbootproject/controllers/UserController.java
+++ springbootproject/src/main/java/com/finalassesment/springbootproject/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.finalassesment.springbootproject.controllers;
 import com.finalassesment.springbootproject.models.User;
 import com.finalassesment.springbootproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,17 +18,25 @@ public class UserController {
     UserService service;
 
     @GetMapping(path = "/get_all")
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return service.getAllUsers();
     }
 
     @GetMapping(path = "/{id}")
-    public User getUser(@PathVariable Long id){
+    public User getUser(@PathVariable Long id) {
         return service.findUserById(id);
     }
 
     @PostMapping()
-    public ResponseEntity<User> create(@RequestBody User user){
+    public ResponseEntity<User> create(@RequestBody User user) {
         return service.save(user);
+    }
+
+    @PutMapping()
+    public ResponseEntity<User> update(@RequestBody User user) {
+        User userInDb = service.findUserById(user.getId());
+        if (!userInDb.toString().equals(user.toString())) {
+            return service.save(user);
+        } else return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
     }
 }
